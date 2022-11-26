ab5c4de614f0dec1d0b6fbb067e3cab78023e164
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-25 21:24:54 -0500

    add router and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  // { path: 'users', component: UsersComponent },
+  // { path: 'contact', component: ContactComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ], exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
