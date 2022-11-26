53e8aed2755f2f4d7a08f67179e4d99e4d8bb6ee
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-25 21:59:49 -0500

    try to fix users get request

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersService } from "./../../services/users.service";
+import { UsersService } from 'src/app/services/users.service';
+
 
 @Component({
   selector: 'app-users',
